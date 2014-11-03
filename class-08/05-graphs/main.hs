290d64fc5074a43ac3dfb402f750b50ed243ddbe
Amuhar <Amuhar3@mail.ru>
2014-11-04 02:35:01 +0400

    main.hs

--- class-08/05-graphs/main.hs
+++ class-08/05-graphs/main.hs
@@ -1,7 +1,7 @@
 import AbstractGraph as AG
 import qualified AdjList as AL
 import Data.List
-
+import System.Environment
 
 createGraph ::FilePath -> IO(Int,Bool,[(Int,Int,Int)])
 createGraph  fname =  do
@@ -11,4 +11,11 @@ createGraph  fname =  do
     let (numbVert,orientation) = (read numbVert'::Int,read orientation'::Bool)
     let edgeList = foldr (\ls acc ->if null ls then acc else (let [b,e,w] = ls in  (read b, read e, read w ):acc))  [] $ tail data'
     return (numbVert,orientation,edgeList)
+	
+	
+main = do
+	[fname] <- getArgs 
+	data' <- createGraph fname
+	let (numbVert,orientation,edgeList) = data'
+	print $( mkGraph orientation (0,numbVert-1) edgeList ::AL.Graph Int Int)
 
