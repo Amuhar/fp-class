dde04751ede4b6ca38a56892dcc2474d8f4e6702
Amuhar <Amuhar3@mail.ru>
2014-12-01 03:31:32 +0400

    task2

--- class-12/02-polish-calc.hs
+++ class-12/02-polish-calc.hs
@@ -23,21 +23,63 @@
 -}
 
 
+
 import Control.Monad
 import Control.Monad.State
+import Control.Monad.Trans
+import Control.Monad.Trans.Maybe
+import Control.Monad.Reader
+import Control.Monad.Writer
+import System.Environment
 
-type Stack = [Int]
 
-push :: Int -> State Stack ()
-push x = get >>= put . (x:)
 
-pop :: State Stack Int
-pop = get >>= \(x:xs) -> put xs >> return x
+type Value = Int
+type Var = String
+type Stack = [(Var,Value)]
 
-evalRPN :: String -> Int
-evalRPN xs = head $ execState (mapM step $ words xs) []
+push :: (Var,Value) -> MaybeT (WriterT (Sum Int) (State Stack)) ()
+push x = tell (Sum 1) >> get >>= put . (x:)
+
+-- 4 19 1 + * +   => 80 +  => Nothing
+-- операции значения => Nothing 
+
+pop :: MaybeT (WriterT (Sum Int) (State Stack)) (Var,Value)
+pop = tell (Sum 1) >> get >>= (\ xs ->  guard (not. null $ xs) >> put (tail xs) >> return (head xs))
+                       
+
+
+evalRPN:: [String] -> (Maybe Int, Int)
+evalRPN  xs = do 
+   let ((value,s) ,state) = (runState (runWriterT (runMaybeT $ mapM step  xs)) []) 
+   case value of 
+        Nothing ->  (Nothing , getSum s)
+        p       -> (if (length state > 1) then (Nothing , getSum s) else (Just (snd . head $ state), getSum s)) -- 7 8 9 6 + - => state = [7,7] возвращаю Nothing
   where
     step "+" = processTops (+)
     step "*" = processTops (*)
-    step  n  = push (read n)
-    processTops op = op `liftM` pop `ap` pop >>= push
+    step "-" = processTops (-)
+    step  n  = let [vr,vl] = words n in push ( vr, read vl)
+    processTops op = do 
+        a <- pop
+        b <- pop
+        let c = snd a `op` snd b
+        push (fst b ,c)
+    
+    
+transformParams :: [String] -> [String]              
+transformParams [] = []                  
+transformParams xs = if (not ( elem (head xs ) ["+","-","*"]  )) then
+                        (head xs ++ " " ++ (head. tail $ xs)) : (transformParams (tail . tail $ xs))
+                     else (head xs):(transformParams (tail xs)) 
+                     
+
+params :: ReaderT [String]  IO(Maybe Int,Int)
+params = do
+    n <-  ask
+    let res = evalRPN (transformParams n)
+    return res
+    
+main = do 
+        p <- getArgs
+        runReaderT params p 
